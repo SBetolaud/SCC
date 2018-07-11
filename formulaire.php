152bf9eb6734aee75351465dd82d25b5e22b4b01
Stéphane <sbetolaud@hotmail.Fr>
2018-07-11 10:44:33 +0200

    Premier commit

---
+++ formulaire.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Formulaire</title>
+
+</head>
+<body>
+
+<?php
+
+echo "<form action = '' method = 'post' class='conneter'>";
+echo "<label for='name'>Entrer son login</label>";
+echo "<input type='text' name='id' id='id'/> "
+echo "<label for = 'mdp'>Entrer son mot de passe</label> ";
+echo "<input type = 'text' name = 'mdp' id='mdp'/>";
+echo "<input type='submit' name = 'soumettre' id='soumettre'/>";
+
+
+id = 
+
+if ($_POST["id"]=="") {
+	# code...
+}
+
+
+?>
+
+
+</body>
+</html>
